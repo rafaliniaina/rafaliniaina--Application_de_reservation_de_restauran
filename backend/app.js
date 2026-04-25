4106b6a97f385379c3a700e775c9dd21b006254e
rafaliniaina <rafaliniaina66@gmail.com>
2026-04-25 21:11:01 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(helmet({ crossOriginResourcePolicy: { policy: 'cross-origin' } }));
 app.use(cors({
-  origin:      process.env.CORS_ORIGIN || 'http://localhost:3000',
+  origin:      process.env.CORS_ORIGIN,
   credentials: true,
   methods:     ['GET','POST','PUT','DELETE','OPTIONS'],
   allowedHeaders: ['Content-Type','Authorization'],
